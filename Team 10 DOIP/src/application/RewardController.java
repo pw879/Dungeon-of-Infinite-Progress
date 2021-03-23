a4dd9301552da5ca46138c7f75f95e978f610c98
huynhn98 <nuffin_@outlook.com>
2021-03-22 20:58:57 -0500

    added functionality of the create button

--- Team 10 DOIP/src/application/RewardController.java
+++ Team 10 DOIP/src/application/RewardController.java
@@ -1,30 +1,59 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class RewardController {
+public class RewardController implements Initializable {
 
 	ObservableList<Reward> shop = FXCollections.observableArrayList();
 	ObservableList<Reward> inventory = FXCollections.observableArrayList();
 	
 	@FXML
-	TableView<Reward> purchaseTable = new TableView<>();
-	TableView<Reward> sellTable = new TableView<>();
-	TextField label;
-	TextField description;
-	TextField cost;
-	
-	
+	public TableView<Reward> purchaseTable = new TableView<>();
+	public TableView<Reward> sellTable = new TableView<>();
+	public TextArea label;
+	public TextArea description;
+	public TextArea cost;
+	public TableColumn<Reward, String> pRewardCol;
+	public TableColumn<Reward, String> pDescriptionCol;
+	public TableColumn<Reward, Integer> pCostCol;
+	public TableColumn<Reward, String> sRewardCol;
+	public TableColumn<Reward, String> sDescriptionCol;
+	public TableColumn<Reward, Integer> sCostCol;
 	
 	public void createReward() {
-		ObservableList<Reward> rewards = FXCollections.observableArrayList();
-		rewards.add(new Reward(label.getText(), description.getText(), Integer.parseInt(cost.getText())));
-		purchaseTable.setItems(rewards);
+		Reward newR = new Reward(label.getText(), description.getText(), Integer.parseInt(cost.getText()));
+		shop.add(newR);
+		
+		
+		
+
+		purchaseTable.setItems(shop);
+		
+		
+		
 	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		
+		pRewardCol.setCellValueFactory(new PropertyValueFactory<Reward, String>("name"));
+		pDescriptionCol.setCellValueFactory(new PropertyValueFactory<Reward, String>("description"));
+		pCostCol.setCellValueFactory(new PropertyValueFactory<Reward, Integer>("cost"));
+		
+	}
+	
+	
 	
 	
 	
